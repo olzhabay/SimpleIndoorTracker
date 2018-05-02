6a5d2624026f8ee619108800110164267d00711e
Olzhas Kaiyrakhmet <olzhabay.i@gmail.com>
2018-05-02 14:27:10 +0900

    init wifi listing

--- app/src/main/java/com/example/olzhas/myapplication/MainActivity.java
+++ app/src/main/java/com/example/olzhas/myapplication/MainActivity.java
@@ -8,25 +8,41 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     SensorManager SM;
     WifiManager wifiManager;
     private LinkedList<float[]> data = new LinkedList<>();
+    ArrayList<HashMap<String, String>> wifiList = new ArrayList<>();
     int samplingPeriodUs = 50000;
     double time = 3;
     double threshold = 0.1;
     String result = "";
-    TextView tv;
+    TextView textStatus;
+    ListView listView;
+    Button buttonRecognize;
+    Button buttonScan;
+    int scanSize = 0;
+    SimpleAdapter adapter;
+
+    String ITEM_KEY = "key";
+    List<ScanResult> scanResults;
 
     private SensorEventListener SEL = new SensorEventListener() {
         @Override
@@ -51,9 +67,8 @@ public class MainActivity extends AppCompatActivity {
     private BroadcastReceiver rssiReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-            wifiManager.startScan();
-            int newRssi = wifiManager.getConnectionInfo().getRssi();
+            scanResults = wifiManager.getScanResults();
+            scanSize = scanResults.size();
         }
     };
 
@@ -66,9 +81,11 @@ public class MainActivity extends AppCompatActivity {
         SM = (SensorManager)getSystemService(SENSOR_SERVICE);
         SM.registerListener(SEL, SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), samplingPeriodUs);
 
-        tv = (TextView)findViewById(R.id.textView);
-        Button bt = (Button)findViewById(R.id.button);
-        bt.setOnClickListener(new Button.OnClickListener(){
+        textStatus = (TextView)findViewById(R.id.textView);
+        listView = (ListView)findViewById(R.id.listView);
+        buttonRecognize = (Button)findViewById(R.id.buttonRecognize);
+        buttonScan = (Button)findViewById(R.id.buttonScan);
+        buttonRecognize.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View v) {
                 double magnitude = 0;
@@ -82,12 +99,40 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     result = "Not move\n";
                 }
-                tv.setText(result);
+                textStatus.setText(result);
+            }
+        });
+        buttonScan.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                wifiList.clear();
+                wifiManager.startScan();
+                Toast.makeText(getApplicationContext(), "Scanning..." + scanSize, Toast.LENGTH_SHORT).show();
+                try {
+                    scanSize = scanSize - 1;
+                    while (scanSize >= 0) {
+                        HashMap<String, String> item = new HashMap<>();
+                        item.put(ITEM_KEY, scanResults.get(scanSize).SSID + " " + scanResults.get(scanSize).capabilities);
+                        wifiList.add(item);
+                        scanSize--;
+                        adapter.notifyDataSetChanged();
+                    }
+                } catch (Exception e) { }
             }
         });
 
-        // wifi
-        registerReceiver(rssiReceiver, new IntentFilter(WifiManager.RSSI_CHANGED_ACTION));
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        if (!wifiManager.isWifiEnabled()) {
+            Toast.makeText(getApplicationContext(), "wifi is disabled... makinng it enable", Toast.LENGTH_LONG).show();
+            wifiManager.setWifiEnabled(true);
+        }
+        adapter = new SimpleAdapter(
+                MainActivity.this,
+                wifiList,
+                R.layout.row,
+                new String[] { ITEM_KEY },
+                new int[] { R.id.list_value});
+        registerReceiver(rssiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
     }
 
     @Override
